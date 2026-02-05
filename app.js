51ebdbb40ba60187c77e28da860c54871f847c25
r-parameshwaran <parameshwaran711@gmail.com>
2026-02-06 00:05:06 +0530

    Namaste node js initial setup and node js overview

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = "Parameshwaran";
+var a = 5;
+var b = 10;
+console.log(name);
+console.log(a + b);
+// console.log(global)
+// console.log(this) -  {} "Empty object"
+console.log(globalThis);
+console.log(globalThis === global);
